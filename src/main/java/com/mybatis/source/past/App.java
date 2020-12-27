300946c5fdac78e9bf1068018892e3c282a31f82
TuYuan <2813305749@qq.com>
2020-12-27 15:47:23 +0800

    Initial commit

---
+++ src/main/java/com/mybatis/source/past/App.java
@@ -0,0 +1,48 @@
+package com.mybatis.source.past;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://119.29.37.86:3306/test?characterEncoding=utf8&serverTimezone=GMT%2B8","root","123456");
+            connection.createStatement();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from user");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+                System.out.println(resultSet.getString("age"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (connection != null) {
+                            connection.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
